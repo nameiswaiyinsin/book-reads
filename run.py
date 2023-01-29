35ecc9a64ab97a42b7752018d767d99282e0648d
nameiswaiyinsin <waiyinsin@gmail.com>
2023-01-29 17:23:04 +0000

    Fix indentation and create add_book() function

--- run.py
+++ run.py
@@ -19,28 +19,28 @@ def menu():
     """
     Display a menu with a list of options to choose from
     """
-print("Welcome to your Book Reads Record diary! \n")
+    print("Welcome to your Book Reads Record diary! \n")
 
-while True:
-    print("""
-        -----Menu-----
-        1. Add Book
-        2. Display All Books 
-        3. Delete Book
-        4. Exit
-        """)
+    while True:
+        print("""
+            -----Menu-----
+            1. Add Book
+            2. Display All Books 
+            3. Delete Book
+            4. Exit
+            """)
 
-    option = input("Please select options from 1-4: ")
-    if option == '1':
-        add_book()
-    elif option == '2':
-        display_all_books()
-    elif option == '3':
-        delete_book()
-    elif option == '4':
-        exit_programme()
-    else:
-        print("Invalid option, please select number from 1-4: ")
+        option = input("Please select options from 1-4: ")
+        if option == '1':
+            add_book()
+        elif option == '2':
+            display_all_books()
+        elif option == '3':
+            delete_book()
+        elif option == '4':
+            exit_programme()
+        else:
+            print("Invalid option, please select number from 1-4: ")
 
 
 def add_book():
@@ -48,7 +48,9 @@ def add_book():
     Add a new book to the google spreadsheet with the accompanying information
     Book Title, Author, Start Date, End Date, Rating, Review
     """
+    book_title = input("Please enter the book title: ")
     
+
 
 
 menu()
