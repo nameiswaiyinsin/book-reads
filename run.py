84adb6107fd6e24324ba77c8109ceab00db1cbc6
nameiswaiyinsin <waiyinsin@gmail.com>
2023-01-29 00:57:09 +0000

    Add elif conditions to loop through options, using the While loop for a repetitive task

--- run.py
+++ run.py
@@ -19,10 +19,24 @@ def menu():
     Display a menu with a list of options to choose from
     """
 print("Welcome to your Book Reads Record diary. \n")
-print("""
-    -----Menu-----
-    1. Add Book
-    2. Display All Books 
-    3. Delete Book
-    4. Exit
-    """)
+
+while True:
+    print("""
+        -----Menu-----
+        1. Add Book
+        2. Display All Books 
+        3. Delete Book
+        4. Exit
+        """)
+
+    option = input("Please select options from 1-4: ")
+    if option == '1':
+        add_book()
+    elif option == '2':
+        display_all_books()
+    elif option == '3':
+        delete_book()
+    elif option == '4':
+        exit_programme()
+    else:
+        print("Invalid option, please select number from 1-4: ")
